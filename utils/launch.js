fe37d326808edfd6fac4f7ef3a91b3103fec2953
Jo <147356717+josolinap@users.noreply.github.com>
2025-08-20 21:45:16 +0800

    Create launch.js
    v1

---
+++ utils/launch.js
@@ -0,0 +1,9 @@
+import puppeteer from 'puppeteer-core';
+
+export async function getBrowser() {
+  return puppeteer.launch({
+    headless: 'new',
+    executablePath: process.env.PUPPETEER_EXEC_PATH || '/usr/bin/google-chrome-stable',
+    args: [
+      '--no-sandbox',
+      '--disable-set
