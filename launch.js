860bd56b826790b9488ba56835efc3ff974eed63
Jo <147356717+josolinap@users.noreply.github.com>
2025-08-20 21:13:18 +0800

    Create launch.js
    kimi v1

---
+++ launch.js
@@ -0,0 +1,19 @@
+// utils/launch.js
+import puppeteer from 'puppeteer-core';
+
+export async function getBrowser() {
+  return puppeteer.launch({
+    headless: 'new',
+    executablePath: process.env.PUPPETEER_EXEC_PATH || '/usr/bin/google-chrome-stable',
+    args: [
+      '--no-sandbox',
+      '--disable-setuid-sandbox',
+      '--disable-dev-shm-usage',
+      '--disable-gpu',
+      '--disable-features=VizDisplayCompositor',
+      '--no-first-run',
+      '--no-zygote',
+      '--single-process'
+    ]
+  });
+}
