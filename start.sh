33f34f82e0e7e2b5b6f5ebc5bee6813120338d8d
Jo <147356717+josolinap@users.noreply.github.com>
2025-08-27 23:29:08 +0800

    Update start.sh
    v

--- start.sh
+++ start.sh
@@ -5,11 +5,11 @@ HOME=/home/$USER
 DISP=:1
 export DISPLAY=$DISP
 
-# 1) X framebuffer
+# 1. start headless X server
 su-exec $USER Xvfb $DISP -screen 0 1280x720x24 -ac +extension RANDR +render -noreset &
-while [ ! -e /tmp/.X11-unix/X${DISP#*:} ]; do sleep 0.1; done
+while [ ! -e /tmp/.X11-unix/X${DISPLAY#*:} ]; do sleep 0.1; done
 
-# 2) Chromium kiosk
+# 2. chromium kiosk
 su-exec $USER chromium-browser \
   --no-sandbox \
   --disable-dev-shm-usage \
@@ -17,5 +17,5 @@ su-exec $USER chromium-browser \
   --window-size=1280,720 \
   "https://duckduckgo.com" &
 
-# 3) serve X11 via noVNC on port 8080
+# 3. expose X11 via websockify + noVNC on port 8080
 exec su-exec $USER websockify --web=/usr/share/novnc 8080 localhost:5901
